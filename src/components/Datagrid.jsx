6186ee853baa045ee1a2ef49bc264a2abac75f97
yash1302 <jadhavyash623@gmail.com>
2024-02-09 14:58:00 +0530

    fix:added new features to admin Dashboard

---
+++ src/components/Datagrid.jsx
@@ -0,0 +1,70 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import { DataGrid } from "@mui/x-data-grid";
+import Button from "@mui/material/Button";
+
+const columns = [
+  { field: "id", headerName: "ID", width: 90 },
+  {
+    field: "Name",
+    headerName: "Name",
+    width: 150,
+    flex: 1,
+    editable: true, 
+  },
+  {
+    field: "Email",
+    headerName: "Email",
+    width: 150,
+    flex: 1,
+    editable: true,
+  },
+  {
+    field: "Profile",
+    headerName: "Profile",
+    renderCell: () => (
+      <strong>
+        {/* <button variant="contained" size="small" style={{ marginLeft: 16 }}>
+            Open
+          </Button> */}
+        <Button variant="contained" size="small" style={{ marginLeft: 16 }}>
+          Contained
+        </Button>
+      </strong>
+    ),
+    flex: 0.5,
+  },
+];
+
+const rows = [
+  { id: 1, Name: "Snow", Email: "Jon" },
+  { id: 2, Name: "Snow", Email: "Jon" },
+  { id: 3, Name: "Snow", Email: "Jon" },
+  { id: 4, Name: "Snow", Email: "Jon" },
+  { id: 5, Name: "Snow", Email: "Jon" },
+  { id: 6, Name: "Snow", Email: "Jon" },
+  { id: 7, Name: "Snow", Email: "Jon" },
+  { id: 8, Name: "Snow", Email: "Jon" },
+  { id: 9, Name: "Snow", Email: "Jon" },
+  { id: 10, Name: "Snow", Email: "Jon" },
+];
+
+export default function Datagrid() {
+  return (
+    <Box sx={{ height: 570, width: "100%" }}>
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        initialState={{
+          pagination: {
+            paginationModel: {
+              pageSize: 8,
+            },
+          },
+        }}
+        pageSizeOptions={[5]}
+        checkboxSelection
+      />
+    </Box>
+  );
+}
--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -1,18 +1,60 @@
-import React from 'react'
-import Navbar from '../components/Navbar'
+import React from "react";
+import Navbar from "../components/Navbar";
+import { styled } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Grid";
+import Material from "../components/Material";
+import Datagrid from "../components/Datagrid";
+
+const info = [
+  {
+    name: "yash",
+    email: "yash@gmail.com",
+  },
+  {
+    name: "yash",
+    email: "yash@gmail.com",
+  },
+  {
+    name: "yash",
+    email: "yash@gmail.com",
+  },
+  {
+    name: "yash",
+    email: "yash@gmail.com",
+  },
+];
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: "center",
+  color: theme.palette.text.secondary,
+}));
 
 export default function AdminPage() {
   return (
     <div>
-      <Navbar/>
-      <div className='flex justify-between h-full'>
-        <div className='border-y-2 border-black w-1/3 h-screen flex justify-center'>
-            <div className='border-2 inline-block justify-center'>Name</div>
-             
-        </div>
-        <div className='border-2 border-black w-1/3 h-screen flex justify-center'>Email</div>
-        <div className='border-y-2 border-black w-1/3 h-screen flex justify-center'>Profile</div>
-      </div>
+      <Navbar />
+      {/* <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={1}>
+          <Grid item xs={4}>
+            <Item className="font-bold">Name</Item>
+          </Grid>
+          <Grid item xs={4}>
+            <Item>Email</Item>
+          </Grid>
+          <Grid item xs={4}>
+            <Item>Profile</Item>
+          </Grid>
+        </Grid>
+      </Box>
+      {info.map((item, idx) => (
+        <Material key={idx} name={item.name} email={item.email} />
+      ))} */}
+      <Datagrid />
     </div>
-  )
+  );
 }
